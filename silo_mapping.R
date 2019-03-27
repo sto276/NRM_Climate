e6f979d1c60316d01a7e54bed88466778099a9f8
Mike Stower <michaelostower@gmail.com>
2019-03-27 16:14:54 +1000

    Allow selection of specific NRM region.

--- silo_mapping.R
+++ silo_mapping.R
@@ -1,22 +1,28 @@
+library(tidyverse)
 library(maptools)
 library(readr)
 library(rgdal)
 
 nrm <- readOGR(dsn = "Maps/NRM_Regions_2017", layer = "NRM_Regions_2017", stringsAsFactors = FALSE)
 
+regions <- nrm@data[["NRM_REGION"]]
+region <- subset(nrm, NRM_REGION == "Condamine")
+
 stations <- read_csv("Maps/AllStations.csv")
 
 map <- ggplot() +
-  geom_polygon(data = nrm,
+  geom_polygon(data = region,
                aes(x = long,
                    y = lat,
                    group = group),
                colour = "black",
                fill = NA)
 
-stats <- ggplot() +
   geom_point(data = stations,
              aes(x = Lon,
                  y = Lat),
              colour = "skyblue",
              size = 0.2)
+  #coord_cartesian(xlim = c(114, 152),
+  #                ylim =c(-43, -12))
+  #labs(title = "Australia by weather station location")
